45643dccbbf78cac3837f0ebc3a68af273a30dc3
manuel martinez <manuel.martinez59000@gmail.com>
2019-11-11 16:07:31 +0100

    second commit - randonw letters array implemented

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,9 @@
 class GamesController < ApplicationController
   def new
+    alphabet_letters = ('A'..'Z').to_a
+    @letters = []
+    @letters << alphabet_letters.sample until @letters.length == 10
+    @letters
   end
 
   def score
